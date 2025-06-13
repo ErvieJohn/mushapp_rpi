f206710c00f807599984ca6af657ad9c43e2bd29
ErvieJohn <erviejohn.villareal79@gmail.com>
2025-06-13 13:20:22 +0800

    Added Color

--- gui/main.py
+++ gui/main.py
@@ -833,20 +833,23 @@ class TabButtonLayout(QWidget):
                     logging.info(datetime.now().strftime('%m-%d-%Y %H:%M:%S Sending data to firebase realtime database...'))
                     print("Sending data to firebase realtime database...")
 
+                    fTemp = float(self.jsonData["temperature"])
+                    iHumid = int(self.jsonData["humidity"])
+                    iCo2 = int(self.jsonData["co2ppm"])
+                    iWater = int(self.jsonData["waterLevel"])
+                    fWater = float(self.jsonData["waterLevel"]/19 * 100)
+
                     self.label1.setText('Temperature: <b><span style="color:{};">{}°C</span></b>'.format(
-                        "green" if self.jsonData["temperature"] >= 20 or self.jsonData["temperature"] <= 30 
-                        else "orange" if self.jsonData["temperature"] <= 19 else "red", self.jsonData["temperature"]))
+                        "green" if fTemp >= 20 or fTemp <= 30 
+                        else "orange" if fTemp <= 19 else "red", self.jsonData["temperature"]))
                     self.label2.setText('Humidity: <b><span style="color:{};">{} %</span></b>'.format(
-                        "green" if  self.jsonData["humidity"] >= 70 or self.jsonData["humidity"] <=85 
-                        else "red", self.jsonData["humidity"]))
+                        "green" if  iHumid >= 70 or iHumid <=85 else "red", self.jsonData["humidity"]))
                     self.label3.setText('CO2: <b><span style="color:{};">{} PPM</span></b>'.format(
-                        "green" if self.jsonData["co2ppm"] >= 400 or self.jsonData["co2ppm"] <= 1000 
-                        else "red", self.jsonData["co2ppm"]))
+                        "green" if iCo2 >= 400 or iCo2 <= 1000 else "red", self.jsonData["co2ppm"]))
                     self.label4.setText("Water Percentage: <b>{:.1f}%</b>".format(
-                        self.jsonData["waterLevel"]/19 * 100))
+                        "red" if fWater < 1 or fWater > 100 else "green", fWater))
                     self.label5.setText('Water Level: <b><span style="color:{};">{}</span></b>'.format(
-                        "red" if self.jsonData["waterLevel"] == 0 or self.jsonData["waterLevel"] >= 20 
-                        else "green", self.jsonData["waterLevel"]))
+                        "red" if iWater == 0 or iWater >= 20 else "green", self.jsonData["waterLevel"]))
 
                     if self.check_internet(): # if there is an internet connection
                         try:
